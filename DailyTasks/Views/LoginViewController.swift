aa2daf42dfb1ed2dd1648e27ce503fee7e38bbb1
Duc Nguyen <108277804+HornSeller@users.noreply.github.com>
2024-03-14 17:31:54 +0700

    ct

--- DailyTasks/Views/LoginViewController.swift
+++ DailyTasks/Views/LoginViewController.swift
@@ -8,15 +8,54 @@
 import UIKit
 import FirebaseAuth
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var subView: UIView!
+    @IBOutlet weak var emailTf: UITextField!
+    @IBOutlet weak var passwordTf: UITextField!
+    @IBOutlet weak var emailTfBackgroundImage: UIImageView!
+    @IBOutlet weak var passwordTfBackgroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
+        subView.layer.cornerRadius = 36
+        
+        emailTf.delegate = self
+        passwordTf.delegate = self
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        
+        subView.addGestureRecognizer(tapGesture)
+        
         FirebaseAuth.Auth.auth().createUser(withEmail: "b@gmail.com", password: "abcxyz") { (result, error) in
-            print(result)
+            print(error)
+        }
+    }
+    
+    @objc func hideKeyboard() {
+        subView.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == emailTf {
+            emailTfBackgroundImage.image = UIImage(named: "emailTf1")
+        } else {
+            passwordTfBackgroundImage.image = UIImage(named: "passwordTf1")
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == emailTf {
+            emailTfBackgroundImage.image = UIImage(named: "emailTf")
+        } else {
+            passwordTfBackgroundImage.image = UIImage(named: "passwordTf")
         }
     }
     
